44e6b3235aa1e2ad74dda5c186254e024d9685e7
M. Saiqul Haq <saiqulhaq@gmail.com>
2015-02-24 08:07:15 +0700

    add indexController#importData action

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -16,5 +16,51 @@ export default Ember.Controller.extend({
       });
     });
     return self.store.all('college');
-  }.property()
+  }.property(),
+  actions: {
+    importData: function(parsedData){
+      "use strict";
+      var defaultGrade = [], courseId = 1, semesterId, semesterRecord, self = this, semesters = [];
+      parsedData.gradePointValue.forEach(function(grade){
+        defaultGrade.pushObject(self.store.createRecord("grade", self.store.normalize("grade", {
+            id: grade.id, letter: grade.letter, point: grade.point
+          })
+        ));
+      });
+      parsedData.data.forEach(function(semester, semesterIndex){
+        semesterId = semesterIndex + 1;
+        semesterRecord = self.store.createRecord("semester",
+          self.store.normalize("semester", {
+            id: semesterId, name: semester.semesterName
+          })
+        );
+        semester.courses.forEach(function(course){
+          semesters.pushObject(self.store.createRecord("course", self.store.normalize("course",
+                {
+                  id: courseId++,
+                  semester: semesterRecord,
+                  name: course.courseName,
+                  ppc: course.creditHour,
+                  grade: defaultGrade.find(function(gradeItem){
+                    return gradeItem.id.toString() === course.grade.toString();
+                  })
+                }
+              )
+            )
+          );
+        });
+      });
+
+      var grade = [];
+      defaultGrade.forEach(function(g){
+        grade.pushObject({
+          id: g.get('id'),
+          letter: g.get('letter'),
+          point: g.get('point')
+        });
+      });
+
+      return this.transitionToRoute("calculators", {queryParams: {parsedGrade: JSON.stringify(grade)}});
+    }
+  }
 });
